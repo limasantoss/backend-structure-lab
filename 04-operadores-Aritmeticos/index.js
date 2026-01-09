806d402f88f0da7834a48f2962f4c481e37aa7d2
Igor Santos <igorlimasantos667@gmail.com>
2026-01-09 18:20:11 -0300

    operadores Aritmeticos

---
+++ 04-operadores-Aritmeticos/index.js
@@ -0,0 +1,11 @@
+const originalPrice = 500;
+const discountPercentage = 15 ;  // 15$
+
+const discontValue = (originalPrice * discountPercentage) / 100
+const finalPrice = originalPrice - discontValue  
+
+console.log(`--Sistema de Vendas---`);
+console.log(`Preço:${originalPrice}`);
+console.log(`Desconto : ${discountPercentage}`)
+console.log(`Total a pagar : ${finalPrice}`)
+
