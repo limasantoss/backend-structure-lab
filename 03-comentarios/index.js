4717f19ae94061bda68ba5baca8da83a9d8ca22b
Igor Santos <igorlimasantos667@gmail.com>
2026-01-09 16:11:33 -0300

    comentarios

---
+++ 03-comentarios/index.js
@@ -0,0 +1,13 @@
+// Aula 03 - Praticando comentarios // 
+
+const job = "Estagiario" // Meu Profissão atual
+
+//console.log(job) não vai aparecer no terminal por que esta comentado // 
+
+console.log(job);
+
+
+//Desafio do calculo //
+const total = 50 + /* ignorar -> - */ 100+ 50    
+console.log(`O total é ${total}`)
+
