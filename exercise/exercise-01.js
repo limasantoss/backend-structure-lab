790f37e63e4c5dea09ee46c1da633150c3f9ee85
Igor Santos <igorlimasantos667@gmail.com>
2026-01-09 16:00:44 -0300

    praticas

--- exercise/exercise-01.js
+++ exercise/exercise-01.js
@@ -4,6 +4,6 @@
    Qual palavra-chave você usaria?
 */
 
-const userName = "Igor Lima Dos Santos";
+const userName = " Igor Lima Dos Santos";
 const course = " Analise e Desenvolvimento de Sistemas-ADS"
 console.log(`Meu nome é ${userName} e faço o curso de ${course}`)
