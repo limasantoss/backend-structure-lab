387c79ae5fcb58b1e0abacd35bc65830f0bffb2d
Igor Santos <igorlimasantos667@gmail.com>
2026-01-09 15:12:38 -0300

    fix: correção

---
+++ 02-variaveis/index.js
@@ -0,0 +1,19 @@
+//1 -Criando uma variavel com const (Não mudam)
+const tech ="Javascript";
+const framework ="React";
+console.log("Estou estudando" + tech + "para usar com " + framework);
+
+//2.0 - Criando variáveis com Let (podem mudar)
+let statusCurso = "Iniciante";
+console.log("Meu nivel atual"+ statusCurso)
+
+//Reatribuindo o Valor 
+statusCurso = "Estagiario em evolução ";
+console.log("Novo status"+statusCurso);
+
+//3 - Praticando boas Praticas(CamelCase e nomes claros)
+const userAge = 20;
+const isIntern = true; 
+const companyName ="DEWAY";
+console.log(`Dados:${userAge} anos, estagiario:${isIntern},empresa:${companyName}`);
+
