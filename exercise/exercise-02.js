c2ba0445989eb778bf66491d9ba39e393b767a73
Igor Santos <igorlimasantos667@gmail.com>
2026-01-09 16:17:38 -0300

    exercise-02

---
+++ exercise/exercise-02.js
@@ -0,0 +1,12 @@
+/*
+  EXERCÍCIO PARA VOCÊ:
+  1. Tente comentar o console.log acima e rodar o arquivo.
+  2. Veja que nada aparecerá no terminal.
+  3. Descomente e mude o valor da variável 'job'.
+*/
+const job = "Estagiario de Software"
+const total = 50 + /* ignorar -> */ 100 + 50; 
+
+//console.log(`O total é: ${total}`); 
+
+console.log(`Trabalho como ${job} | total calculado ${total}`)
