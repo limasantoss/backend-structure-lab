79961821fc3fceacccf903bc18988b96e9b330d0
Igor Santos <igorlimasantos667@gmail.com>
2026-01-09 18:20:34 -0300

    exercise

---
+++ exercise/exercise-03.js
@@ -0,0 +1,10 @@
+// 2. Desafio do Tempo (Conversão)
+const minutosDoMes = 30 * 24 * 60 ;
+console.log(`Um mês de 30 dias tem ${minutosDoMes} minutos`) //Dias * horas * minutos 
+console.log(minutosDoMes)
+
+//03 -Modulo (Resto)
+const resto = 15%4
+console.log(`O resto da Divisão de 15 divido por 4 é ${resto}`)
+
+//O que você acha de tentarmos um exercício onde você calcula o seu ganho por hora baseado no seu horário de estagiário (das 08:00 às 13:00)?
