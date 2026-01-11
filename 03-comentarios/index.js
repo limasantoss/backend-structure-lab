bfea52bc80d86834213b1ebc5a51428547d2c2bf
Igor Santos <igorlimasantos667@gmail.com>
2026-01-10 22:55:29 -0300

    comentarios

--- 03-comentarios/index.js
+++ 03-comentarios/index.js
@@ -7,7 +7,7 @@ const job = "Estagiario" // Meu Profissão atual
 console.log(job);
 
 
-//Desafio do calculo //
+//Desafio do calculo//
 const total = 50 + /* ignorar -> - */ 100+ 50    
 console.log(`O total é ${total}`)
 
