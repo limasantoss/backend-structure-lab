45a8ff1ad0d9e39d5d3316fe6830393b89b8e3e1
Igor Santos <igorlimasantos667@gmail.com>
2026-01-12 11:13:12 -0300

    index.js]

---
+++ 05-operador-de-atribuicao-simples/index.js
@@ -0,0 +1,10 @@
+// == atribuição simples
+// += adição simples
+// *= multiplicação simples
+// /-= Divisão combinada 
+
+let price = 300 ;
+console.log(price)
+
+price += price + 3;
+console.log(price)
