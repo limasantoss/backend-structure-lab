90835fe14c0bbdbfeb906bece64d2afada0ae57c
Igor Santos <igorlimasantos667@gmail.com>
2026-01-12 15:30:08 -0300

    index.js

--- 06-operadores-logicos/index.js
+++ 06-operadores-logicos/index.js
@@ -3,3 +3,15 @@
 // && E (AND)
 // || OU (OR)
 // ! NÃO (NOT)
+
+const a = true
+const b = false 
+console.log(a && b)
+
+
+const lavarLouca = false;
+const fazerDever = false; 
+console.log(lavarLouca || fazerDever)
+
+const isOpen = false
+console.log(!isOpen)
