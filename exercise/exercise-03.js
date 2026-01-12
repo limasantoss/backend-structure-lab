2c78c22a4037752d1d3f81d2e5746dae83cda298
Igor Santos <igorlimasantos667@gmail.com>
2026-01-12 11:59:22 -0300

    exercise-03

--- exercise/exercise-03.js
+++ exercise/exercise-03.js
@@ -7,4 +7,3 @@ console.log(minutosDoMes)
 const resto = 15%4
 console.log(`O resto da Divisão de 15 divido por 4 é ${resto}`)
 
-//O que você acha de tentarmos um exercício onde você calcula o seu ganho por hora baseado no seu horário de estagiário (das 08:00 às 13:00)?
