74888143897190ce3f280dc86b749093a91e4ab5
Igor Santos <igorlimasantos667@gmail.com>
2026-01-12 11:59:11 -0300

    index.js

--- 05-operador-de-atribuicao-simples/index.js
+++ 05-operador-de-atribuicao-simples/index.js
@@ -3,8 +3,23 @@
 // *= multiplicação simples
 // /-= Divisão combinada 
 
-let price = 300 ;
-console.log(price)
+let x = 10 ; 
+x += 3 ; 
+// x  = x + 3 
+console.log(x)
 
-price += price + 3;
-console.log(price)
+let s = "10";
+s += 5;
+// s = s + 5
+// Observação aqui esta concatenando pois o 10 esta como string 
+console.log(s)
+
+
+let a = 2
+a = a + 3;
+a = a + 4 
+console.log(a)
+
+let p = 300 ;
+p = p + 3  
+console.log(p)
