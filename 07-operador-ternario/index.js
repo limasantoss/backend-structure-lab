22c6f910898c76e54548782751dadde2d035951d
Igor Santos <igorlimasantos667@gmail.com>
2026-01-12 21:31:58 -0300

    teste

---
+++ 07-operador-ternario/index.js
@@ -0,0 +1,14 @@
+const number = 5 ;
+console.log((number % 2  === 0 ) ?"Par" : "Impar");
+
+
+const nome = "Igor";
+console.log((nome === "Igor")?"Sim" : "Não");
+
+const idade = 22;
+console.log((idade === 22)?"maior de idade": "Menor de Idade");
+
+const aprendeuJs = true;
+const aprendeuComRino = true;
+
+console.log((aprendeuComRino && aprendeuJs)?"Aprendeu com Rino" : "Não aprendeu com Rino")
